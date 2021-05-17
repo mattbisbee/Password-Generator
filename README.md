71bf1f3411f8b3f1a9bf9c3187a3b479540eea15
Biznaz <aldhelm7@gmail.com>
2021-05-16 19:17:33 -0700

    added README and deleted old code

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Password Generator Starter Code
+
+## Generating a Random Password Based on User Input #
+---
+### **General Info:**
+The task of this project was to create an interface where the user can input certain criteria in order to generate a random strong password. The criteria for the generated passwords were to have the option to choose numeric values, special, lower, and uppercase characters. At least 1 option must be selected. The password that is created has to span from 8 characters to 128 characters; no more, no less. When all the specific criteria has been chosen, the password should then display for the user.
+
+
+### **Have a Selection of Criteria That The User Can Choose**
+I have included an inital prompt screen to get the amount of characters the user wants. I included "if' statements to ensure the user entered the appropriate amount of characters. If the correct amount wasn't chosen an alert window opens to let the user know what the critera is and that they need to start over by clicking the button again. After the password length selection, there is a series of confirm windows to select the type of characters the user would like to use. I included an "if" statement at the end to ensure the user chooses at least 1 of the options. Again, if they choose none of the options the code will stop running and a message will appear in a pop up screen telling the user to start over by clicking the button again. There is also a message that pops into the password box that states "try again".
+
+### **Final Notes**
+The first iteration of the code I wrote was not working. Despite reaching out to the TA's and trying to get it to work I just could not get the password to generate with the amount of characters the user selected. The TA suggested revisiting the format I chose to use and try to rework it differently. I looked at a number of sources online and what had been covered in class and on the slack threads. I decided to follow a simpler format and to wrap the entire code in a single function instead of using mulitple functions (I ran into a lot of scoping problems with that). At first I tried to use seperate functions to continue the prompts. That information was then sent to an array that captured all that data. When I tried to code a "for loop" I just couldn't get it all to work properly and return a password. 
+
+In the new format the prompts are variables instead of functions and they continue to run on their own. I set up arrays for each character type as well. I tried to use ".String.fromCharCode" as I had in the previous version but again I could not get this to render correctly so I just typed all the characters out. When the user selects the type of characters that will be used in the password, those choices are then added together into the empty array (using the spread method) through some 'if' statements which is then looped through the 'for' loop and returned into the generated password.
+
+### **Location:**
+Website can be found at :
+
+GitHub: 
+
+## **Images:**
+![Screenshot](assets/capture.JPG)
--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 function generatePassword() {
 var randomSpecialOpt = ['~', '!',',','@','#','$','%','^','&','*','(',')','{','}','[',']','<','>',',','.','/',':',"'"];
 var randomNumberOpt = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
@@ -10,42 +9,45 @@ var availChar = [];
 //Prompt for input
 passwordLength = prompt('What is the desired length of your password?');
 if (passwordLength <8 || passwordLength >128 || passwordLength === null || passwordLength === "") {
-  alert('You need to provide a valid response!');
+  alert('Password length must be between 8 - 128 characters long! Please click "GENERATE PASSWORD" button again to retry.');
+  return ('Try Again');
 }
 
 var specialChar = confirm ('Click OK to confirm including special characters.');
-console.log(specialChar);
 
 var numberChar = confirm('Click OK to confirm including numeric characters.');
-console.log(numberChar);
 
 var lowerCase = confirm('Click OK to confirm including lowercase characters.');
-console.log(lowerCase);
 
 var upperCase = confirm('Click OK to confirm including uppercase characters.')
-console.log(upperCase);
 
-//CCollecting user desired characters
+if (specialChar === false && numberChar === false && lowerCase === false && upperCase=== false) {
+  alert ('You must choose at least 1 option to form a valid password. Please click "GENERATE PASSWORD" button again to retry.');
+  return('Try Again');
+}
+
+
+//Collection and concatenation of user desired characters
 if (specialChar) {
-  availChar = availChar.concat(randomSpecialOpt);
+  availChar = [...availChar, ...randomSpecialOpt];
 }
 if (numberChar) {
-  availChar = availChar.concat(randomNumberOpt);
+  availChar = [...availChar, ...randomNumberOpt];
 }
 if (lowerCase) {
-  availChar = availChar.concat(upperCaseOpt);
+  availChar = [...availChar, ...upperCaseOpt];
 }
 if (upperCase) {
-  availChar = availChar.concat(lowerCaseOpt);
+  availChar = [...availChar, ...lowerCaseOpt];
 }
 
 //password generation
-  var generatePassword = ""
+  var genPassword = "";
   for (let i = 0; i < passwordLength; i++) {
-    var randomGen = [Math.floor(Math.random() * availChar.length)];
-    generatePassword = generatePassword + availChar[randomGen];
+    var randomGen = Math.floor(Math.random() * availChar.length);
+    genPassword = genPassword + availChar[randomGen];
   }
-  return generatePassword;
+  return genPassword;
 }
 
 // Get references to the #generate element
@@ -60,87 +62,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-/*
-//Assignment code here
-var userPasswordLength = 0;
-
-function promptMe(){
-  var passwordLength = prompt('What is the desired length of your password?');
-  if (passwordLength === null || passwordLength === "") {
-    alert('You need to provide a valid response!');
-    return promptMe(); 
-  } else if (passwordLength < 8) {
-    alert('You need to have more than 8 characters. Please try again!');
-    return promptMe();
-  } else if (passwordLength >128) {
-    alert('You cannot have more than 128 characters. Please try again!');
-    return promptMe();
-  } else {
-    for (var i = 0; i < passwordLength; i++) {
-      console.log(passwordLength);
-    }
-    userPasswordLength = passwordLength;
-    specChar();
-  }
-}
-
-function specChar () {
-  var specialChar = confirm('Click OK to confirm including special characters.')
-  if (specialChar === false) {
-    numChar();
-  } else if (specialChar === true) {
-    window.myArray.push(String.fromCharCode(Math.floor(Math.random() *15) + 33));
-    console.log(specialChar);
-    numChar();
-  }
-}
-
-function numChar () {
-  var numberChar = confirm('Click OK to confirm including numeric characters.')
-  if (numberChar === false) {
-    
-  } else if (numberChar === true) {
-    window.myArray.push(String.fromCharCode(Math.floor(Math.random() *10) +48));
-    console.log(numberChar);
-    lowCase();
-  }
-}
-
-function lowCase() {
-  var lowerCase = confirm('Click OK to confirm including lowercase characters.')
-  if (lowerCase === false) {
-    upCase();
-  } else if (lowerCase === true) {
-    window.myArray.push(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
-    console.log(lowerCase);
-    upCase();
-  }
-}
-
-function upCase () {
-  var upperCase = confirm('Click OK to confirm including uppercase characters.')
-  if (upperCase === false) {
-  } else if (upperCase === true) {
-    window.myArray.push(String.fromCharCode(Math.floor(Math.random()* 26) +65));
-    console.log(upperCase);
-  }
-  passwordDisplay.innerHTML = myArray.join('');
-}
-
-var upperCase = String.fromCharCode(Math.floor(Math.random()* 26) +65);
-console.log(upperCase);
-var lowerCase = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-console.log(lowerCase);
-var randomNumber = String.fromCharCode(Math.floor(Math.random() *10) +48);
-console.log(randomNumber);
-var randomSpecial = String.fromCharCode(Math.floor(Math.random() *15) + 33);
-console.log(randomSpecial);
-
-
-var passwordDisplay = document.querySelector("#password")
-
-var myArray=[];
-
-*/
-
-
